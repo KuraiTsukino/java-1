c747e6dc5907be30aaee4c83bfda4d76399884b4
Kurai Tsukino <sandym3000@hotmail.com>
2022-01-31 01:37:02 -0600

    Tabla de multiplicar con while

---
+++ src/Main3.java
@@ -0,0 +1,11 @@
+public class Main3 {
+    
+    public static void main(String [] args) {
+        
+        int c = 0;
+        while (c <=10) {
+            System.out.println("El valor de c es: " + c);    
+            c++;
+        }
+    }
+}
