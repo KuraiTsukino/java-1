f5918bd011fd575c800f9c7d7f7218e3358565c1
Kurai Tsukino <sandym3000@hotmail.com>
2022-01-31 00:48:36 -0600

    Condicional anidado

--- src/Espar.java
+++ src/Espar.java
@@ -8,10 +8,23 @@ public class Espar {
         System.out.print("Ingrese un numero entero: ");
         int n = leer.nextInt();
 
-        if (n % 2 == 0) {
-            System.out.println("Es un numero par");
+        if (n != 0) {
+            if (n > 0) {
+                if (n % 2 == 0 ) {
+                    System.out.printf("El numero %d es Par positivo\n", n);
+                } else {
+                    System.out.printf("El numero %d es Impar positivo\n", n);
+                }
+            } else {
+                if (n % 2 == 0 ) {
+                    System.out.printf("El numero %d es Par negativo\n", n);
+                } else {
+                    System.out.printf("El numero %d es Impar negativo\n", n);
+                }
+            }
         } else {
-            System.out.println("Es un numero impar");
+            System.out.printf("El numero %d es Neutro\n", n);
         }
+
     }
 }
