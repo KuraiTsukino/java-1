5ffb5ed285270f3431b4564bf87357ca6a55471b
Kurai Tsukino <sandym3000@hotmail.com>
2022-01-31 00:28:36 -0600

    if en java

---
+++ src/Main2.java
@@ -0,0 +1,13 @@
+import javax.net.ssl.TrustManagerFactory;
+
+public class Main2 {
+   
+    public static void main(String [] args) {
+        
+        if(true) {
+            System.out.println("Se cumple la condicion");
+        } else {
+            System.out.println("No se cumple la condicion");
+        }
+    }
+}
